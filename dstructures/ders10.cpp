f89e9a78a5d90b33ac05766f607100250ac7a443
can61cebi <can61cebi@gmail.com>
2022-12-07 11:41:27 +0300

    11:41 PM

--- dstructures/ders10.cpp
+++ dstructures/ders10.cpp
@@ -11,4 +11,11 @@ Eleman ekleme maliyeti: log2tabanN, Cikarma: N*log2tabanN
 1. Construction Phase: O*(log2tabanN)
 2. Sorting Phase: O*(N*log2tabanN)
 O*(N*log2tabanN + N*log2tabanN) = O*(2*Nlog2tabanN).
+Siralama (Sort) Yontemleri
+1. Insertion Sort (Araya eklemeli) O(N^2)
+2. Bubble Sort (Kabarcik siralama) O(N^2)
+3. Selection Sort O(N^2)
+4. BST ile siralama O(N*log2tabanN + N) = O(N*log2tabanN)
+5. Heap Sort O(2*Nlog2tabanN)
+6. Quick Sort 7. Shell Sort 8. Merge Sort 9. Radix Sort 10. Silly Sort
 */
