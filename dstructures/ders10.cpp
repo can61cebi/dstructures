0fa222d18e4e7686d93795e964f6a35e538131e1
can61cebi <can61cebi@gmail.com>
2022-12-07 11:08:53 +0300

    11:04 PM

---
+++ dstructures/ders10.cpp
@@ -0,0 +1,14 @@
+/*
+HEAP ne ise yarar ?	O(1): Formul
+Sort ve dynamic allocation
+Heap ne kullanilarak implement edilir? (kodlanir?)	O(N): Vektoru gezmek
+Heap ile stack, queue, BST, LL arasindaki fark nedir?	O(N^2): Matris gezme
+F(N) = N^30 + 7*N + N + 500 // Polinomial
+F(N) = fac(N) + 3 // Non Polinomial Problem
+F(N) = 2^N + N^2 + N + 5 // N.P.
+Effective: etkili, Efficiency: verimlilik
+Eleman ekleme maliyeti: log2tabanN, Cikarma: N*log2tabanN
+1. Construction Phase: O*(log2tabanN)
+2. Sorting Phase: O*(N*log2tabanN)
+O*(N*log2tabanN + N*log2tabanN) = O*(2*Nlog2tabanN).
+*/
